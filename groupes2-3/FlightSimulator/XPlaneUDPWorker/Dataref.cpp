4a62c68b00d291c29ab7ed230e5fe62435b59caa
LE CAM <nico_77510@yahoo.com>
2016-03-18 10:55:02 +0100

    Mise à jour XplaneUDPWorker/Dataref

--- groupes2-3/FlightSimulator/XPlaneUDPWorker/Dataref.cpp
+++ groupes2-3/FlightSimulator/XPlaneUDPWorker/Dataref.cpp
@@ -1,41 +1,9 @@
 #include "Dataref.h"
 
-void setup() {
 
-// ALTIMETRE
-struct dref_struct_in QAltimeter ;
-QAltimeter.dref_freq=100;
-QAltimeter.dref_id=1;
-QAltimeter.dref_path=;
-
-// ANENOMETRE
-struct dref_struct_in QAirSpeed ;
-QAirSpeed.dref_freq=100;
-QAirSpeed.dref_id=2 ;
-QAirSpeed.dref_path="sim/cockpit2/gauges/indicators/airspeed kts pilot";
-
-// VARIOMETRE
-struct dref_struct_in QVariometer ;
-QVariometer.dref_freq=100;
-QVariometer.dref_id=3;
-QVariometer.dref_path= "sim/cockpit2/gauges/indicators/airspeed acceleration kts sec pilot";
-
-// GYROSCOPE DIRECTIONNEL
-struct dref_struct_in QGyro ;
-QGyro.dref_freq=100 ;
-QGyro.dref_id=4 ;
-QGyro.dref_path="sim/cockpit2/gauges/indicators/compass heading deg mag";
-
-// HORIZON ARTIFICIEL
-struct dref_struct_in QHA ;
-QHA.dref_freq=100 ;
-QHA.dref_id=5 ;
-QHA.dref_path="sim/cockpit2/gauges/actuators/artificial horizon adjust deg pilot" ;
-
-// TURN COORDINATOR
-struct dref_struct_in QTC ;
-QTC.dref_freq=100 ;
-QTC.dref_id=6 ;
-QTC.dref_path="sim/cockpit2/gauges/indicators/slip deg" ;
-
+Dataref::Dataref (int freq=100, int id, QString* path)
+{
+	this->dref_freq=freq;
+	this->dref_id=id;
+	this->dref_path=path;
 }
--- groupes2-3/FlightSimulator/XPlaneUDPWorker/Dataref.h
+++ groupes2-3/FlightSimulator/XPlaneUDPWorker/Dataref.h
@@ -1,2 +1,16 @@
 #ifndef DATAREF_H
 #define DATAREF_H
+
+class Dataref
+{
+private:
+	int dref_freq ;
+	int dref_id ;
+	QString* dref_path ;
+
+public:
+	Dataref(int freq=100, int id, QString* path);
+
+} ;
+
+#endif
